ea728d0e84b1cbbdef22d697767e2cd67908d44c
Ty <tdrygg@gmail.com>
2019-08-24 19:17:26 -0500

    Add files
    Add files to store python script

---
+++ PyBank/main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 15 23:45:34 2019
+
+@author: tdryg
+"""
+import csv
+
+with open('..\\Resources\\budget_data.csv') as csvpath:
+    print(csvpath)
---
+++ PyPoll/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 15 20:41:37 2019
+
+@author: tdryg
+"""
+
